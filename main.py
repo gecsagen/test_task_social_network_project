0955b3f8179bead75180bb85e07dd713f6d9b7d7
Charlie <geksbomba@gmail.com>
2023-07-06 13:29:32 +0600

    add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.routing import APIRouter
+
+from user.api import user_router
+from user.api_login import login_router
+from post.api import post_router
+
+# create instance of the app
+app = FastAPI(title="Social network API", version="1.0.0")
+
+# create the instance for the routes
+main_api_router = APIRouter()
+
+# set routes to the app instance
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+main_api_router.include_router(login_router, prefix="/login", tags=["login"])
+main_api_router.include_router(post_router, prefix="/post", tags=["post"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    # run app on the host and port
+    uvicorn.run(app, host="0.0.0.0", port=8000)
