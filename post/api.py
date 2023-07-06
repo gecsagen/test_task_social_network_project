3500e936602f049316c1919a93d48108f7b5f511
Charlie <geksbomba@gmail.com>
2023-07-06 13:27:08 +0600

    add api for delete, update, get post

--- post/api.py
+++ post/api.py
@@ -45,3 +45,70 @@ async def create_post(
     except IntegrityError as err:
         logger.error(err)
         raise HTTPException(status_code=503, detail=f"Database error: {err}")
+
+
+#  удаление поста
+@post_router.delete("/", response_model=PostDeleteResponse)
+async def delete_post(
+    post_id: UUID,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+) -> PostDeleteResponse:
+    #  вызывается функция удаления поста
+    deleted_post_id = await _delete_post(post_id, current_user.user_id, db)
+    if deleted_post_id is None:
+        raise HTTPException(
+            status_code=404, detail=f"Post with id {post_id} not found."
+        )
+    return PostDeleteResponse(deleted_post_id=deleted_post_id)
+
+
+#  получение поста по id
+@post_router.get("/", response_model=ShowPost)
+async def get_post_by_id(
+    post_id: UUID,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+) -> ShowPost:
+    #  вызывается функция получения поста по id
+    post = await _get_post_by_id(post_id, db)
+    if post is None:
+        raise HTTPException(
+            status_code=404, detail=f"Post with id {post_id} not found."
+        )
+    return post
+
+
+#  обновление поста
+@post_router.patch("/", response_model=UpdatedPostResponse)
+async def update_post_by_id(
+    post_id: UUID,
+    body: UpdatePostReuest,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+) -> UpdatedPostResponse:
+    updated_post_params = body.dict(exclude_none=True)
+    #  проверка переданы ли какие-то параметры для изменения поста
+    if updated_post_params == {}:
+        raise HTTPException(
+            status_code=422,
+            detail="At least one parameter for post update info should be provided",
+        )
+    #  вызывается функция получения поста по id и затем проверка наличия поста
+    post = await _get_post_by_id(post_id, db)
+    if post is None:
+        raise HTTPException(
+            status_code=404, detail=f"Post with id {post_id} not found."
+        )
+    try:
+        #  вызывается функция обновления поста
+        updated_post_id = await _update_post(
+            updated_post_params=updated_post_params,
+            post_id=post_id,
+            author_id=current_user.user_id,
+            session=db,
+        )
+    except IntegrityError as err:
+        logger.error(err)
+        raise HTTPException(status_code=503, detail=f"Database error: {err}")
+    return UpdatedPostResponse(updated_post_id=updated_post_id)
